d4620bf7a95bfa7ada02ca4f7e272d105e63708c
Hemel <reaz.hemel@gmail.com>
2024-12-21 01:11:06 +0600

    updated the my application page

--- src/pages/MyApplications.jsx
+++ src/pages/MyApplications.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import useAuth from "../hooks/AuthInfo";
+import { MdDelete } from "react-icons/md";
+import { div } from "motion/react-client";
 
 
 const MyApplications = () => {
@@ -13,9 +15,52 @@ const MyApplications = () => {
     })
   },[user.email])
   return (
-    <div>
-     <h2>My Applications:{jobs.length}</h2>
-    </div>
+   <div>
+        <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Category</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        jobs.map(job => <tr>
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle h-12 w-12">
+                <img
+                  src={job.company_logo}/>
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">{job.company}</div>
+              <div className="text-sm opacity-50">{job.location}</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          {job.title}
+          <br />
+          <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+        </td>
+        <td>{job.category}</td>
+        <th>
+          <button className="btn btn-ghost text-blue-400 text-2xl btn-xs"><MdDelete /></button>
+        </th>
+      </tr>)
+      }
+      
+    </tbody>
+    
+  </table>
+</div>
+   </div>
   );
 };
 
